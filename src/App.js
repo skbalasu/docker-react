38c6a67576918276733781bd1fda4989aa67cf3a
Senthil Kumar Balasubramaniam <skbalasu@extremenetworks.com>
2021-10-22 18:22:01 +0530

    main branch

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>Main Bye there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Modified on feature branch and then trigger pull request.Learn React
         </a>
       </header>
     </div>
