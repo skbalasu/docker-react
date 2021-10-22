22e8e9821a49972d83607ec99810235166b5637d
Senthil Kumar Balasubramaniam <skbalasu@extremenetworks.com>
2021-10-22 19:10:55 +0530

    cool first docker program

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Cool ... My first web docker program!</p>
+        <p>Cool .. . My first web docker program!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
